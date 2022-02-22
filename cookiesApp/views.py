9841d64f3f8129d04ee64c576716af91db11b662
CloudFoundry276 <cloudfoundry276@gmail.com>
2022-02-22 23:10:37 +0530

    session using cookies managed in this app

--- cookiesApp/views.py
+++ cookiesApp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def home(request):
+    request.session.set_test_cookie()
+    return HttpResponse("<h2>Home Page</h2>")
+
+
+def page2(request):
+    if request.session.test_cookie_worked():
+        print("Cookies are enabled!")
+        request.session.delete_test_cookie()
+    return HttpResponse("<h2>Page 2</h2>")
--- djangoCookies/urls.py
+++ djangoCookies/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from cookiesApp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', views.home),
+    path('page2/', views.page2),
 ]
